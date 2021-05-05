f6d2eb5702104632056a8a2b5828a2cb6e714aa7
Athipat Pingwong <athipat.pingwong@mail.kmutt.ac.th>
2021-05-06 01:42:51 +0700

    Spring initializr

---
+++ src/main/java/int221/project/Porject221/Porject221Application.java
@@ -0,0 +1,13 @@
+package int221.project.Porject221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Porject221Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Porject221Application.class, args);
+	}
+
+}
---
+++ src/test/java/int221/project/Porject221/Porject221ApplicationTests.java
@@ -0,0 +1,13 @@
+package int221.project.Porject221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Porject221ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
