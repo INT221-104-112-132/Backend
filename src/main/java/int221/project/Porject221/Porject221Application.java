5fc74a849baa85fb09f8e2b727810e7a0f4d1246
Athipat Pingwong <athipat.pingwong@mail.kmutt.ac.th>
2021-05-10 20:23:40 +0700

    edit

--- src/main/java/int221/project/Porject221/Porject221Application.java
+++ src/main/java/int221/project/Porject221/Porject221Application.java
@@ -2,8 +2,12 @@ package int221.project.Porject221;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import int221.project.Porject221.storageservices.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({StorageProperties.class})
 public class Porject221Application {
 
 	public static void main(String[] args) {
--- src/main/java/int221/project/Porject221/storageservices/StorageProperties.java
+++ src/main/java/int221/project/Porject221/storageservices/StorageProperties.java
@@ -5,8 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix ="smartphone.strorage")
 public class StorageProperties {
 
-
-    private String location ;
+    private String location;
 
     public String getLocation() {
         return location;
